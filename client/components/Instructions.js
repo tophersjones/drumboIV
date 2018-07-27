66d8d551d04ccd18b26aaddc96fd95e89b62db04
Chris <tophersjones@gmail.com>
2018-07-27 14:19:48 -0500

    commit

--- client/components/Instructions.js
+++ client/components/Instructions.js
@@ -11,13 +11,13 @@ const Instructions = () => {
       1. Choose your instrument (Snare, Kick Drum...)
     </div>
     <div>
-      2. Click a cell once to arm the drum machine, click again to disarm
+      2. Click a cell to arm the cell with the selected instrument, click again to disarm
     </div>
     <div>
-      3. Switch instruments to add as many as you'd like
+      3. Switch instruments to arm as many cells as you'd like
     </div>
     <div>
-      4. Press Spacebar or click start to begin playing!
+      4. Press Spacebar or click Start to begin playing! You can arm and disarm cells as Drumbo is playing
     </div>
   </div>
   )
